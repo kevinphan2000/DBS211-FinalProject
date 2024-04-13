9835fde871023f6f9f9ac7ae8910f897e765160b
Phan Trung Kien <kevinkien2706@gmail.com>
2024-04-13 01:32:37 -0400

    Update The New Tables

--- FinalAssignment.sql
+++ FinalAssignment.sql
@@ -31,3 +31,45 @@ CREATE TABLE Payment (
     FOREIGN KEY (LeaseID) REFERENCES Lease_Agreement(LeaseID)
 )
 
+
+CREATE TABLE Tenants (
+    TenantID INT IDENTITY(1,1) PRIMARY KEY,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    ContactNumber VARCHAR(15) CHECK (ContactNumber LIKE '[0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]'),
+    CreditScore INT NOT NULL,
+    Email VARCHAR(100) NOT NULL
+)
+
+
+CREATE TABLE LeaseAgreement (
+    LeaseID INT PRIMARY KEY,
+    unitID INT NOT NULL,
+    agentID INT NOT NULL,
+    StartDate DATE NOT NULL,
+    EndDate DATE NOT NULL,
+    FOREIGN KEY (unitID) REFERENCES Unit(unitID),
+    FOREIGN KEY (agentID) REFERENCES Employee(employeeID)
+)
+
+
+CREATE TABLE MaintenanceRequests (
+    requestID INT PRIMARY KEY,
+    unitID INT NOT NULL, 
+    reportTo INT NOT NULL,
+    requestDate DATE NOT NULL,
+    issueDescription VARCHAR(MAX) NOT NULL,
+    vendorID INT NOT NULL,
+    cost INT NOT NULL,
+    status VARCHAR(100) CHECK (status IN ('Open', 'In Progress', 'Closed')),
+    FOREIGN KEY (vendorID) REFERENCES Vendor(vendorID),
+    FOREIGN KEY (reportTo) REFERENCES Employee(employeeID)
+);
+
+
+CREATE TABLE Vendors (
+    vendorID INT PRIMARY KEY,
+    serviceType VARCHAR(25) DEFAULT 'Cleaning',
+    phone VARCHAR(11) NOT NULL,
+    email VARCHAR(50) NOT NULL
+)
